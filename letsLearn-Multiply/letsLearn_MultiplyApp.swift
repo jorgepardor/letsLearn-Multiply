5abcfb07a17d1558abbc28f390f64b2ff50d8f53
Jorge Pardo <hi@jorgepardo.dev>
2023-10-15 10:27:08 +0200

    Initial Commit

---
+++ letsLearn-Multiply/letsLearn_MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  letsLearn_MultiplyApp.swift
+//  letsLearn-Multiply
+//
+//  Created by Jorge Pardo on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct letsLearn_MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
